e92230a89da88c38f7791f410fb11406d9a228e9
camiloNunez <71714264@ucsm.edu.pe>
2021-07-16 17:41:34 -0500

    2da actualización a la master

--- src/SWork/POO/Carrera.java
+++ src/SWork/POO/Carrera.java
@@ -2,10 +2,59 @@ package SWork.POO;
 
 public class Carrera {
 	String NombreCarrera;
-	String Tipo;
+	int Tipo;
 	String NombreUniversidad;
-	String Estado;
+	boolean Estado;
 	public Carrera() {
 		
+	}
+	public String ObtenerEstado() {
+		if(Estado == true) {
+			return "terminado";
+		}
+		else {
+			return "en curso";
+		}
+	}
+	public String ObtenerTipoProfesional() {
+		if(Tipo == 1) {
+			return "Tecnico";
+		}
+		if(Tipo == 2) {
+			return "Ingeniero";
+		}
+		if(Tipo == 3) {
+			return "Licenciado";
+		}
+		else {
+			return "Tipo Profesional no especificado";
+		}
+	}
+	public String getNombreCarrera() {
+		return NombreCarrera;
+	}
+	public void setNombreCarrera(String nombreCarrera) {
+		NombreCarrera = nombreCarrera;
+	}
+	public int getTipo() {
+		return Tipo;
+	}
+	public void setTipo(int tipo) {
+		Tipo = tipo;
+	}
+	public String getNombreUniversidad() {
+		return NombreUniversidad;
+	}
+	public void setNombreUniversidad(String nombreUniversidad) {
+		NombreUniversidad = nombreUniversidad;
+	}
+	public boolean getEstado() {
+		return Estado;
+	}
+	public void setEstado(boolean estado) {
+		Estado = estado;
+	}
+	public String toString() {
+		return "Carrera: "+this.NombreCarrera+"Centro de Estudios: "+this.NombreUniversidad+"Tipo de Profesional: "+this.ObtenerTipoProfesional()+"Estado: "+this.ObtenerEstado();
 	}
 }
