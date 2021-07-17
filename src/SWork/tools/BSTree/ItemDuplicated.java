42e85c3d62db1bb280f332fd3c34799526c922a9
camiloNunez <71714264@ucsm.edu.pe>
2021-07-16 19:44:56 -0500

    3ra actualización a la master

---
+++ src/SWork/tools/BSTree/ItemDuplicated.java
@@ -0,0 +1,9 @@
+package SWork.tools.BSTree;
+public class ItemDuplicated extends Exception {
+	public ItemDuplicated(String msg) {
+		super(msg);
+	}
+	public ItemDuplicated() {
+		super();
+	}
+}
---
+++ src/SWork/tools/Colas/ExceptionIsEmpty.java
@@ -0,0 +1,10 @@
+package SWork.tools.Colas;
+
+public class ExceptionIsEmpty extends Exception {
+    public ExceptionIsEmpty(String msg) {
+        super(msg);
+    }
+    public ExceptionIsEmpty() {
+        super();
+    }
+}
